bf186d23e8603c522d50c2f688b915592dd3c194
Arthur Lemos <arthur.wagner.lemos@gmail.com>
2025-11-21 21:32:44 -0300

    Refactor Firebase admin initialization with env variables

--- src/config/firebase-admin.ts
+++ src/config/firebase-admin.ts
@@ -1,11 +1,14 @@
 import * as admin from "firebase-admin";
 import { initialize } from "fireorm"
-import serviceAccount from "../../firebase.json"
 
 if(!admin.apps.length){
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-        databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+        credential: admin.credential.cert({
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY,
+            projectId: process.env.FIREBASE_PROJECT_ID
+        } as admin.ServiceAccount),
+        databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
     })
 }
 
